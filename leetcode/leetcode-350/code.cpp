816ed76df6bb05ee1342ce461f4a69313942b1ad
sellen <sellenzh@gmail.com>
2023-08-29 21:37:04 +0800

    23-08-29

---
+++ leetcode/leetcode-350/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int len1 = nums1.size(), len2 = nums2.size();
+        vector<int> res;
+        for (int i = 0; i < len1; i++){
+            
+        }
+    }
+};
